5cd4e2e8db47b2e2a977c5f55f56f80e2fbc9186
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2015-09-27 11:19:16 +0200

    Adding the start of the OnSave infrastructure.

--- src/main/java/org/netbeans/ts/JSOnSaveHook.java
+++ src/main/java/org/netbeans/ts/JSOnSaveHook.java
@@ -1,65 +1,82 @@
 package org.netbeans.ts;
 
-import java.util.logging.Logger;
+import java.util.concurrent.atomic.AtomicBoolean;
 import javax.swing.text.Document;
 import org.netbeans.api.editor.mimelookup.MimeRegistration;
-import org.netbeans.api.editor.mimelookup.MimeRegistrations;
-//import org.netbeans.api.project.FileOwnerQuery;
-import org.netbeans.api.project.Project;
-//import org.netbeans.modules.csl.api.DataLoadersBridge;
-//import org.netbeans.modules.css.prep.util.CssPreprocessorUtils;
 import org.netbeans.spi.editor.document.OnSaveTask;
-//import org.openide.filesystems.FileObject;
-import org.openide.util.RequestProcessor;
+import org.netbeans.ts.options.TypeScriptOptionsPanel;
+import org.openide.awt.StatusDisplayer;
+import org.openide.filesystems.FileObject;
+import org.openide.loaders.DataObject;
+import org.openide.util.NbPreferences;
 
-@MimeRegistrations({
-    @MimeRegistration(mimeType = "text/x-ts", service = OnSaveTask.Factory.class, position = 2000),})
-public class JSOnSaveHook implements OnSaveTask.Factory {
+public class JSOnSaveHook implements OnSaveTask {
 
-    private static final RequestProcessor RP = new RequestProcessor(JSOnSaveHook.class);
-    private static final Logger LOG = Logger.getLogger(JSOnSaveHook.class.getSimpleName());
-    
-    @Override
-    public OnSaveTask createTask(OnSaveTask.Context context) {
-        Document doc = context.getDocument();
-//        final FileObject fileObject = DataLoadersBridge.getDefault().getFileObject(doc);
-//        final Project project = fileObject == null ? null : FileOwnerQuery.getOwner(fileObject);
-        final Project project = null;
-        return project == null ? null : new OnSaveTask() {
-            private boolean cancelled;
+    private final Document document;
 
-            @Override
-            public void performTask() {
-                if (!cancelled) {
-                    //run the CssPreprocessorUtils.processSavedFile() out of the original thread
-                    RP.post(new Runnable() {
+    private final AtomicBoolean canceled = new AtomicBoolean();
 
-                        @Override
-                        public void run() {
-//                            String mimeType = fileObject.getMIMEType();
-//                            CssPreprocessorType type = CssPreprocessorType.find(mimeType);
-//                            if (type == null) {
-//                                // #244470
-//                                LOG.log(Level.WARNING, "Cannot find CssPreprocessorType for MIME type {0} (filename: {1})", new Object[] {mimeType, fileObject.getNameExt()});
-//                                return;
-//                            }
-//                            CssPreprocessorUtils.processSavedFile(project, type);
-//                            LOG.log(Level.INFO, "processSavedFile called for {0} type on project {1}.", new Object[]{type.getDisplayName(), project.getProjectDirectory().getPath()});
-                        }
-                        
-                    });
-                }
-            }
-
-            @Override
-            public void runLocked(Runnable run) {
-                run.run(); //better keep running in the original thread as the tasks may be nested
-            }
-
-            @Override
-            public boolean cancel() {
-                return cancelled = true;
-            }
-        };
+    public JSOnSaveHook(Document doc) {
+        this.document = doc;
     }
+
+    @Override
+    public void performTask() {
+        StatusDisplayer.getDefault().setStatusText("Transpile to TypeScript...",10);
+        String tsc = NbPreferences.forModule(TypeScriptOptionsPanel.class).get("tsc", "");
+        execute(tsc, getFileObject(document));
+    }
+
+    public FileObject getFileObject(Document doc) {
+        Object sdp = doc.getProperty(Document.StreamDescriptionProperty);
+        if (sdp instanceof FileObject) {
+            return (FileObject) sdp;
+        }
+        if (sdp instanceof DataObject) {
+            return ((DataObject) sdp).getPrimaryFile();
+        }
+        return null;
+    }
+
+    public Integer execute(final String path, FileObject fo) {
+        Integer get = null;
+//        ExecutionDescriptor descriptor = new ExecutionDescriptor().
+//                showProgress(true).
+//                frontWindow(true).
+//                controllable(true);
+//        ExternalProcessBuilder processBuilder = new ExternalProcessBuilder(path)
+//                .addArgument("-o").addArgument(fo.getParent().getPath() + "/" + fo.getName() + ".dart.js")
+//                .addArgument("--minify")
+//                .addArgument(fo.getPath());
+//        ExecutionService service = ExecutionService.newService(processBuilder, descriptor, path);
+//        Future<Integer> task = service.run();
+//        try {
+//            get = task.get();
+//        } catch (InterruptedException ex) {
+//            Exceptions.printStackTrace(ex);
+//        } catch (ExecutionException ex) {
+//            Exceptions.printStackTrace(ex);
+//        }
+        return get;
+    }
+
+    @Override
+    public void runLocked(Runnable r) {
+        r.run();
+    }
+
+    @Override
+    public boolean cancel() {
+        canceled.set(true);
+        return true;
+    }
+
+    @MimeRegistration(mimeType = "text/x-ts", service = OnSaveTask.Factory.class, position = 1500)
+    public static final class FactoryImpl implements Factory {
+        @Override
+        public OnSaveTask createTask(Context context) {
+            return new JSOnSaveHook(context.getDocument());
+        }
+    }
+
 }
